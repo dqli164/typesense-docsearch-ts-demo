57341354e6628e2f0169be1bb31bbc33548b38d4
daqiangli <dqli161@163.com>
2023-07-17 20:25:01 +0800

    add typesense typescript demo

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <link
+      rel='stylesheet'
+      href='https://cdn.jsdelivr.net/npm/typesense-docsearch-css@0.3.0'
+    >
+    <div class="searchbar"></div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { docsearch } from 'typesense-docsearch.js/dist/esm/docsearch'
+docsearch({
+  container: '#searchbar',
+  typesenseCollectionName: 'faq',
+  typesenseServerConfig: {
+    nodes: [{
+      host: 'localhost',
+      port: 8108,
+      protocol: 'http'
+    }],
+    apiKey: 'xyz'
+  },
+  typesenseSearchParameters: {
+    q: 'u',
+    query_by: 'url'
+  }
+})
+</script>
